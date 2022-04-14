6a4f1040711a3e899d1284179a544f7468f50c88
Ian Cook <ianscook19@gmail.com>
2022-04-13 22:38:35 -0400

    Testing...

--- basketball/src/index.js
+++ basketball/src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Wardell from './Wardell';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App/>}/>
+        <Route path="/wardell" element={<Wardell/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
