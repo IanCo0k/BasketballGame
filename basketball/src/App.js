22a57d826aa97bb6647b906729a70088761e830a
Ian Cook <ianscook19@gmail.com>
2022-04-13 23:31:15 -0400

    homepage

--- basketball/src/App.js
+++ basketball/src/App.js
@@ -2,11 +2,31 @@ import React from 'react';
 import Wardell from './Wardell';
 import Testing from './Testing';
 import Route from 'react-router-dom';
+import {Card} from 'react-bootstrap';
+import styles from './App.css'
 
 export default function App(){
   return(
-    <div>
-    <a href="/#/wardell">ClicK EM</a>
+    <>
+    <div className={'container'}>
+      <div className={'header_container'}>
+      <h1 className={'header_text'}>Wardell</h1>
+      </div>
     </div>
+
+    <div className={'card_container'}>
+      <Card>
+      <Card.Title>Articles</Card.Title>
+      </Card>
+
+      <a href="/#/wardell">
+      <Card>
+      <Card.Title>Wardell</Card.Title>
+      </Card>
+      </a>
+    </div>
+    </>
+
+
   )
 }
