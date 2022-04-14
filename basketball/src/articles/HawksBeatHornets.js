3bd2aa2cb5e0238b1fbabcb875a8765134f268a1
Ian Cook <ianscook19@gmail.com>
2022-04-14 11:15:44 -0400

    privacy error fixes

--- basketball/src/articles/HawksBeatHornets.js
+++ basketball/src/articles/HawksBeatHornets.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import {Card} from 'react-bootstrap';
-
-export default function HawksBeatHornets(){
-  return(
-    <>
-    <div className={'container'}>
-      <div className={'header_container'}>
-      <h1 className={'header_text'}>Hawks OOKA BOOKA On Hornets</h1>
-      </div>
-    </div>
-
-    <div className={'card_container'}>
-      <h1>Lamelo OOKA BLOAT! </h1>
-      <p>------------</p>
-      <h1>Trae OOKA GOAT!</h1>
-
-    </div>
-    </>
-  )
-}
+// import React from 'react'
+// import {Card} from 'react-bootstrap';
+//
+// export default function HawksBeatHornets(){
+//   return(
+//     <>
+//     <div className={'container'}>
+//       <div className={'header_container'}>
+//       <h1 className={'header_text'}>Hawks OOKA BOOKA On Hornets</h1>
+//       </div>
+//     </div>
+//
+//     <div className={'card_container'}>
+//       <h1>Lamelo OOKA BLOAT! </h1>
+//       <p>------------</p>
+//       <h1>Trae OOKA GOAT!</h1>
+//
+//     </div>
+//     </>
+//   )
+// }
